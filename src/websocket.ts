279b04027cdbe4fc67486ec701c59206ac9ebb71
lexmarcos <marcosuel.vieira@hotmail.com>
2023-07-01 23:59:11 -0300

    feat: Adiciona o evento de entrar em times

--- src/websocket.ts
+++ src/websocket.ts
@@ -3,9 +3,14 @@ import { io } from "./index.js";
 import { verifyToken } from "./utils/verifyToken.js";
 import { v4 as uuidv4 } from "uuid";
 
+export interface IPlayerStats {
+  goals: number;
+}
+
 export interface IPlayer {
   socketId: string;
   username: string;
+  stats: IPlayerStats;
 }
 
 export enum GameStatus {
@@ -14,12 +19,19 @@ export enum GameStatus {
   FinishedGame = "finished game",
 }
 
+export interface ITeam {
+  players: IPlayer[];
+}
+
 export interface IRoom {
   room: string;
   numberOfPlayers: 2 | 4 | 6 | 8;
   typeOfGame: "classic" | "withPowerUps";
   duration: number;
-  players: IPlayer[];
+  teams: {
+    blue: ITeam;
+    red: ITeam;
+  };
   gameStatus: GameStatus;
   id: string;
 }
@@ -31,8 +43,9 @@ interface IRooms {
 export let rooms: IRooms = {};
 
 const createRoom = (roomData: IRoom) => {
+  roomData.gameStatus = GameStatus.WaitingPlayers;
   rooms[roomData.id] = roomData;
-  io.emit("roomCreated", roomData);
+  io.to("lobby").emit("roomCreated", roomData);
 };
 
 export const setupSocket = (io: any) => {
@@ -52,31 +65,68 @@ export const setupSocket = (io: any) => {
     });
 
     socket.on("joinLobby", () => {
-      socket.join("lobby");
       console.log("user joined to lobby");
+
+      socket.join("lobby");
       socket.emit("allRooms", rooms);
     });
 
-    socket.on("joinRoom", ({ room, username }) => {
-      console.log(room);
+    const checkIfPlayerExistisInTeam = (room: string, team: string, username: string) => {
+      return rooms[room].teams[team].players.find(
+        (player: { username: string }) => player.username === username
+      );
+    };
+
+    const checkIfPlayerExistisInSomeTeam = (room: string, username: string) => {
+      return (
+        checkIfPlayerExistisInTeam(room, "blue", username) ||
+        checkIfPlayerExistisInTeam(room, "red", username)
+      );
+    };
+
+    const insertPlayerInTeam = (room: string, team: string, username: string) => {
+      const player: IPlayer = {
+        socketId: socket.id,
+        username: username,
+        stats: {
+          goals: 0,
+        },
+      };
+      rooms[room].teams[team].players.push(player);
+      console.log(`user ${username} joined room ${room} on team ${team}`);
+      io.to(room).emit("joinedToTeam", rooms[room]);
+      io.to("lobby").emit("roomUpdated", rooms[room]);
+      const totalPlayers =
+        rooms[room].teams.blue.players.length + rooms[room].teams.red.players.length;
+      if (totalPlayers === rooms[room].numberOfPlayers) {
+        rooms[room].gameStatus = GameStatus.Playing;
+        io.to(room).emit("startGame");
+      }
+    };
+
+    socket.on("joinTeam", ({ room, team, username }) => {
       if (!rooms[room]) {
         socket.emit("error", "Room does not exist");
-      } else if (rooms[room].players.length >= rooms[room].numberOfPlayers) {
-        socket.emit("error", "Room is full");
+      } else if (rooms[room].gameStatus !== GameStatus.WaitingPlayers) {
+        socket.emit("error", "Game already started");
+      } else if (rooms[room].teams[team].players.length >= rooms[room].numberOfPlayers / 2) {
+        socket.emit("error", "Team is full");
+      } else if (checkIfPlayerExistisInTeam(room, team, username)) {
+        socket.emit("error", "Username already exists in this team");
+      } else if (checkIfPlayerExistisInSomeTeam(room, username)) {
+        socket.emit("error", "Username already exists in some team");
       } else {
-        socket.join(room);
-        socket.emit("joinedRoom", rooms[room]);
-        rooms[room].players.push({ socketId: socket.id, username });
-        console.log("user joined room ", room);
-        if (rooms[room].players.length === rooms[room].numberOfPlayers) {
-          rooms[room].gameStatus = GameStatus.Playing;
-          io.to(room).emit("startGame");
-        }
+        insertPlayerInTeam(room, team, username);
       }
     });
 
+    socket.on("joinRoom", ({ room }) => {
+      console.log("user joined room", room);
+      socket.join(room);
+      socket.emit("joinedRoom", rooms[room]);
+    });
+
     socket.on("message", (data) => {
-      console.log(data);
       io.to(data.room).emit("newMessage", {
         text: data.text,
         author: data.author,
